afb1c58d845c0ea3467ff260c4ea1620ed9eb6a3
ImCitizen13 <meltohamy@protonmail.com>
2023-08-14 14:35:37 +0400

    Added date fomat and content

--- src/components/TweetCard.tsx
+++ src/components/TweetCard.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import { Tweet } from "./InfiniteTweetList";
 import { ProfileImage } from "./ProfileImage";
 
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
+
 export function TweetCard({
   id,
   user,
@@ -16,7 +20,7 @@ export function TweetCard({
         <ProfileImage src={user.image} />
       </Link>
       <div className="flex flex-grow flex-col">
-        <div className="flex gap-1">
+        <div className="flex gap-1"> 
           <Link
             href={"/profiles/" + user.id}
             className="font-bold hover:underline focus-visible:underline"
@@ -24,8 +28,11 @@ export function TweetCard({
             {user.name}
           </Link>
           <span className="text-gray-500">-</span>
-          <span className="text-gray-500">{createdAt}</span>
+          <span className="text-gray-500">
+            {dateTimeFormatter.format(createdAt)}
+          </span>
         </div>
+        <p className="whitespa-pre-wrap">{content}</p>
       </div>
     </li>
   );
